1d13b8af740d5895301893c359106bd718950f0c
Doug Hawkinson <doug5solas@gmail.com>
2018-03-06 15:09:23 -0800

    Setup start script and heroku port

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const hbs     = require('hbs');
 const fs      = require('fs');
 
+const port    = process.env.PORT || 3000;
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
@@ -63,6 +64,6 @@ app.get('/bad', (req, res) => {
 });
 
 
-app.listen(3000, () => {
-    console.log('All the magic happens on port 3000');
+app.listen(port, () => {
+    console.log(`All the magic happens on port ${port}`);
 });
